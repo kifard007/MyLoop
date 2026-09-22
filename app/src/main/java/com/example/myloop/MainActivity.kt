5b0539b4888eb669eeb49fba24a84ccd3399114a
kifard007 <161320917+kifard007@users.noreply.github.com>
2026-09-22 17:24:06 +0700

    Blank project

---
+++ app/src/main/java/com/example/myloop/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.myloop
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+
+}
+
+@Preview(showBackground = true, widthDp = 360, heightDp = 640)
+@Composable
+fun MainScreenPreview() {
+    MaterialTheme {
+        MainScreen()
+    }
+}
